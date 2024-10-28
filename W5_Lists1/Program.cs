9186ad54add8104a9e1e23f5c52cb5bc0c520533
AenuHub <jesuskane92@gmail.com>
2024-10-28 15:04:19 +0300

    add list implementation

--- W5_Lists1/Program.cs
+++ W5_Lists1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> invitees = new List<string>();
+        invitees.Add("Illidan Stormrage");
+        invitees.Add("Slyvanas Windrunner");
+        invitees.Add("Garrosh Hellscream");
+        invitees.Add("Kael'thas Sunstrider");
+        invitees.Add("Malfurion Stormrage");
+        invitees.Add("Tyrande Whisperwind");
+
+        Console.WriteLine("** Invitees **");
+        for (int i = 0; i < invitees.Count; i++)
+        {
+            Console.WriteLine($"{i + 1} - {invitees[i]}");
+        }
+        
+        Console.ReadKey();
+    }
+}
